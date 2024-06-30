ec449aef11e204704b77ab6806f03693b8d71ecb
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 07:00:20 -0700

    Update question_a.py

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,6 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def get_sum_of_evens(num):
+   
+    sum_of_evens = 0
+    for i in range(2, num + 1, 2): 
+        sum_of_evens += i
+    return sum_of_evens
