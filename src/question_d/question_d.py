a9143e9526cc060fd25499d2097587514dfdca16
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 07:02:20 -0700

    Update question_d.py

--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,11 @@
-#write functions here, don't add input('') statements here!
+def get_day_of_week(day):
+    days_of_week = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday"
+    }
+    return days_of_week.get(day) #write functions here, don't add input('') statements here!
