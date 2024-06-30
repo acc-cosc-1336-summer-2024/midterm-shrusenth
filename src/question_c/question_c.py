ce20623caf111758b86aaa212b4bcda80da03401
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 07:01:43 -0700

    Update question_c.py

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,9 @@
-#write functions here, don't add input('') statements here!
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
