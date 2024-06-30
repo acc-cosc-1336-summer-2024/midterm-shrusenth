eafb84d36e1296638091f1cc2b9a6d69d1b34e66
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 07:02:03 -0700

    Update main.py

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,20 @@
-#add import
+from src.question_d.question_d import get_day_of_week
+def main():
+
+    while True:
+        try:
+            day_number = int(input("Enter a number between 1 and 7 : "))
+        except ValueError:
+            print("Invalid Input")
+            continue
+
+        if 1 <= day_number <= 7:
+            day_name = get_day_of_week(day_number)
+            print("The corresponding day of the week is:", day_name)
+            break
+        else:
+            print("Not in range.")
+
+
+if __name__ == "__main__":
+    main()#add import
