1187e5408fd40e0a0eeea44576c3721cd26e9095
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 07:01:08 -0700

    Update question_b.py

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,11 @@
-#write functions here, don't add input('') statements here!
+import question_b
+from src.question_b.question_b import get_fahrenheit
+
+def main ():
+    print("Celsius\t\tFahrenheit")
+    print("-------------------------")
+
+for celsius in range(0, 21):  
+    fahrenheit = get_fahrenheit(celsius)
+    print(f"{celsius}\t\t{fahrenheit:.1f}")  
+    #write functions here, don't add input('') statements here!
