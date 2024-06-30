c139c8ed80626384ea5269e93b48fe8c6db5507c
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-30 06:23:09 -0700

    Update main.py

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,22 @@
-#add import
+import question_a
+from src.question_a.question_a import get_sum_of_evens
+
+def main():
+    while True:
+        num_str = input()
+        if num_str.lower() == 'quit':
+            break
+        
+        if num_str.isdigit():
+            num = int(num_str)
+            if num <= 0:
+                print("Enter a positive integer greater than 0.")
+                continue
+            
+            result = get_sum_of_evens(num)
+            print({result})
+        else:
+            print("Invalid input.")
+
+if __name__ == "__main__":
+    main()#add import
